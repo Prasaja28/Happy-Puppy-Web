b133f882a2d6e474ee05ec054ade177d5a28e972
salsabilananda <salsabilananda222@gmail.com>
2021-12-22 00:00:56 +0700

    update

---
+++ database/migrations/2021_12_20_152813_create_kota_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKotaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kota', function (Blueprint $table) {
+            $table->increments('kota_id');
+            $table->string('nama_kota',50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kota');
+    }
+}
---
+++ database/migrations/2021_12_21_143849_create_formpendaftaranwaralaba_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFormpendaftaranwaralabaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('formpendaftaranwaralaba', function (Blueprint $table) {
+            $table->increments('waralaba_id');
+            $table->string('nama',50);
+            $table->boolean('jenis_kelamin');
+            $table->integer('umur');
+            $table->string('pendidikan',20);
+            $table->string('alamat_resmi',100);
+            $table->string('kota_resmi', 20);
+            $table->string('provinsi_resmi', 20);
+            $table->string('telp_kantor', 15);
+            $table->string('email_resmi', 64);
+            $table->string('telp_rumah', 15);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('formpendaftaranwaralaba');
+    }
+}
