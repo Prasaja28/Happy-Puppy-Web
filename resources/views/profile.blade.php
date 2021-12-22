06064e7aaabcbe4b9957473b06a25efcb0b39e95
farahdilaup <94000765+farahdilaup@users.noreply.github.com>
2021-12-23 01:01:45 +0700

    Update profile.blade.php
    profile baru

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4,15 +4,107 @@
 
 @section('css-internal')
 <!-- Add tag css in here -->
+ <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+
+
 
 @endsection
 
 @section('konten')
 <!-- add konten in here -->
-<h1>ini halaman profile</h1>
+<br><br><br>
+ <main id="main">
+    <!-- ======= Story Intro Section ======= -->
+    <section id="story-intro" class="story-intro">
+      <div class="container">
+
+        <div class="row">
+
+          <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
+            <h3>PROFIL PERUSAHAAN</h3>
+            <p class="fst-italic">
+              HAPPY PUPPY - PELOPOR KARAOKE KELUARGA DI INDONESIA
+            </p>
+            <p>
+              Masyarakat Indonesia adalah masyarakat religius yang gemar bernyanyi, berhak untuk mendapatkan sarana hiburan bernyanyi yang bebas dari simbol-simbol hiburan malam. Pasar inilah yang kemudian ditangkap dengan jeli oleh wirausahawan Santoso Setyadji.
+<br><br>
+              Konsep Karaoke Keluarga untuk pertama kalinya diperkenalkan di Indonesia oleh Santoso dengan didirikannya Happy Puppy Self-Service Family Karaoke Box pada tanggal 14 Nopember 1992 di Surabaya.
+<br><br>
+              Oleh karena konotasi "Karaoke" di Indonesia sudah demikian identiknya dengan hiburan malam, Santoso menambahkan kata "Family" di depan kata Karaoke Box sebagai upaya penekanan bahwa hiburan yang disediakan adalah hiburan yang baik untuk keluarga atau hiburan untuk orang yang baik-baik.
+<br><br>
+              Karaoke Keluarga mendapat sambutan yang baik dari masyarakat Indonesia. Dari Surabaya, Santoso mengembangkan jaringan Karaoke Keluarga ke Jakarta, Semarang, Samarinda, Balikpapan, Makassar, Yogjakarta, Cibubur, Pontianak, Manado, Serpong dan sebentar lagi Banjarmasin.
+<br><br>
+              Pada bulan Januari 2004, Santoso Setyadji dan Happy Puppy Karaoke Keluarga dianugerahi Sertifikat Museum Rekor Indonesia (MURI) sebagai Pelopor Karaoke Keluarga di Indonesia.
+            </p>
+          </div>
+                     	<br><br><br>
+           <div class="col-lg-6 order-6 order-lg-6">
+           	           	<br><br>
+            <img src="img/hp1.jpg" class="img-fluid" alt="">
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Story Intro Section -->
+
+    <!-- ======= Members Section ======= -->
+    <br><br><br><br>
+          <section id="members" class="members">
+      <div class="container">
+      	 <div class="col-lg-12 pt-4 pt-lg-0 order-2 order-lg-1 content center">
+            <h3>FASILITAS</h3><br>
+            <p>
+              Happy Puppy menghadirkan hiburan karaoke keluarga sejak 1992. Dilengkapi dengan kualitas audio video yg prima, koleksi lagu yang lengkap, peralatan yg canggih, sistem layar sentuh, desain ruangan yang modern dan berbagai fasilitas lainnya untuk memberikan pengalaman bernyanyi yang tak terlupakan..
+<br><br>
+
+	<div class="bubbleInfo1">
+				
+					<img class="trigger" src="img/1.jpg" width=200 class = "hotspot" onmouseover="tooltip.show('<table><tr>	<td><table>	<tr>		<td><table width=450 height=120 cellpadding=5 cellspacing=5>			<tr>				<td valign=top rowspan=2><img src=img/1.jpg width=140/></td>				<td>					<div style =font-weight:bold;font-family:arial;font-size:16px;text-align:justify;color:#ffffff;>Video Berkualitas Full HD					</div>				</td>			</tr>			<tr>				<td valign=top>					<div style =font-family:arial;font-size:12px;text-align:justify;color:#ffffff;line-height:18px;>Setiap video klip dari lagu-lagu yang tersedia, ditampilkan dalam video dengan gambar yang baik dan berkualitas Full HD.					</div>				</td>			</tr>			</table>		</td>	</tr>    </table></td></tr></table>');" onmouseout="tooltip.hide();"/>
+				
+				
+				
+					<img class="trigger" src="img/2.jpg" width=200 class = "hotspot" onmouseover="tooltip.show('<table><tr>	<td><table>	<tr>		<td><table width=450 height=120 cellpadding=5 cellspacing=5>			<tr>				<td valign=top rowspan=2><img src=img/2.jpg width=140/></td>				<td>					<div style =font-weight:bold;font-family:arial;font-size:16px;text-align:justify;color:#ffffff;>Sistem Suara					</div>				</td>			</tr>			<tr>				<td valign=top>					<div style =font-family:arial;font-size:12px;text-align:justify;color:#ffffff;line-height:18px;>Setiap ruangan dilengkapi dengan sistem suara terbaik buatan Jepang dan kualitas suara yang prima.					</div>				</td>			</tr>			</table>		</td>	</tr>    </table></td></tr></table>');" onmouseout="tooltip.hide();"/>
+				
+				
+				
+					<img class="trigger" src="img/3.jpg" width=200 class = "hotspot" onmouseover="tooltip.show('<table><tr>	<td><table>	<tr>		<td><table width=450 height=120 cellpadding=5 cellspacing=5>			<tr>				<td valign=top rowspan=2><img src=img/3.jpg width=140/></td>				<td>					<div style =font-weight:bold;font-family:arial;font-size:16px;text-align:justify;color:#ffffff;>Minimart					</div>				</td>			</tr>			<tr>				<td valign=top>					<div style =font-family:arial;font-size:12px;text-align:justify;color:#ffffff;line-height:18px;>Belanja makanan ringan, minuman dan suvenir  di Minimart untuk belanja lebih hemat.					</div>				</td>			</tr>			</table>		</td>	</tr>    </table></td></tr></table>');" onmouseout="tooltip.hide();"/>
+				
+				
+					<img class="trigger" src="img/4.jpg" width=200 class = "hotspot" onmouseover="tooltip.show('<table><tr>	<td><table>	<tr>		<td><table width=450 height=120 cellpadding=5 cellspacing=5>			<tr>				<td valign=top rowspan=2><img src=img/4.jpg width=140/></td>				<td>					<div style =font-weight:bold;font-family:arial;font-size:16px;text-align:justify;color:#ffffff;>TV Lebih Lebar					</div>				</td>			</tr>			<tr>				<td valign=top>					<div style =font-family:arial;font-size:12px;text-align:justify;color:#ffffff;line-height:18px;>Setiap ruangan menggunakan TV LED layar lebar, antara lain TV 42 inch &amp; TV 65 Inch dengan jumlah TV yang disesuaikan dengan kapasitas ruangan.					</div>				</td>			</tr>			</table>		</td>	</tr>    </table></td></tr></table>');" onmouseout="tooltip.hide();"/>
+				
+				
+					<img class="trigger" src="img/5.jpg" width=200 class = "hotspot" onmouseover="tooltip.show('<table><tr>	<td><table>	<tr>		<td><table width=450 height=120 cellpadding=5 cellspacing=5>			<tr>				<td valign=top rowspan=2><img src=img/5.jpg width=140/></td>				<td>					<div style =font-weight:bold;font-family:arial;font-size:16px;text-align:justify;color:#ffffff;>Koleksi Lagu					</div>				</td>			</tr>			<tr>				<td valign=top>					<div style =font-family:arial;font-size:12px;text-align:justify;color:#ffffff;line-height:18px;>Lebih dari 60.000 lagu yang tersedia antara lain lagu Indonesia, lagu Barat,lagu Mandarin, lagu Korea, lagu Jepang, lagu India dan lain lain.					</div>				</td>			</tr>			</table>		</td>	</tr>    </table></td></tr></table>');" onmouseout="tooltip.hide();"/>
+				
+				
+					<img class="trigger" src="img/6.jpg" width=200 class = "hotspot" onmouseover="tooltip.show('<table><tr>	<td><table>	<tr>		<td><table width=450 height=120 cellpadding=5 cellspacing=5>			<tr>				<td valign=top rowspan=2><img src=img/6.jpg width=140/></td>				<td>					<div style =font-weight:bold;font-family:arial;font-size:16px;text-align:justify;color:#ffffff;>Makanan & Minuman					</div>				</td>			</tr>			<tr>				<td valign=top>					<div style =font-family:arial;font-size:12px;text-align:justify;color:#ffffff;line-height:18px;>Menyajikan berbagai jenis makanan dan minuman favorit untuk dinikmati saat karaoke.					</div>				</td>			</tr>			</table>		</td>	</tr>    </table></td></tr></table>');" onmouseout="tooltip.hide();"/>
+				
+				
+					<img class="trigger" src="img/7.jpg" width=200 class = "hotspot" onmouseover="tooltip.show('<table><tr>	<td><table>	<tr>		<td><table width=450 height=120 cellpadding=5 cellspacing=5>			<tr>				<td valign=top rowspan=2><img src=img/7.jpg width=140/></td>				<td>					<div style =font-weight:bold;font-family:arial;font-size:16px;text-align:justify;color:#ffffff;>Desain Tematik					</div>				</td>			</tr>			<tr>				<td valign=top>					<div style =font-family:arial;font-size:12px;text-align:justify;color:#ffffff;line-height:18px;>Ruangan karaoke dilengkapi dengan desain interior yang unik dan menarik. Setiap room menggunakan desain yang berbeda dan tematik.					</div>				</td>			</tr>			</table>		</td>	</tr>    </table></td></tr></table>');" onmouseout="tooltip.hide();"/>
+				
+				
+					<img class="trigger" src="img/8.jpg" width=200 class = "hotspot" onmouseover="tooltip.show('<table><tr>	<td><table>	<tr>		<td><table width=450 height=120 cellpadding=5 cellspacing=5>			<tr>				<td valign=top rowspan=2><img src=img/8.jpg width=140/></td>				<td>					<div style =font-weight:bold;font-family:arial;font-size:16px;text-align:justify;color:#ffffff;>Sistem Layar Sentuh					</div>				</td>			</tr>			<tr>				<td valign=top>					<div style =font-family:arial;font-size:12px;text-align:justify;color:#ffffff;line-height:18px;>Pemilihan lagu menggunakan sistem layar sentuh . sentuh secara skrol dan sliding dan pilih langsung.					</div>				</td>			</tr>			</table>		</td>	</tr>    </table></td></tr></table>');" onmouseout="tooltip.hide();"/>
+				
+				
+					<img class="trigger" src="img/9.jpg" width=200 class = "hotspot" onmouseover="tooltip.show('<table><tr>	<td><table>	<tr>		<td><table width=450 height=120 cellpadding=5 cellspacing=5>			<tr>				<td valign=top rowspan=2><img src=img/9.jpg width=140/></td>				<td>					<div style =font-weight:bold;font-family:arial;font-size:16px;text-align:justify;color:#ffffff;>Volume Suara Ternormalisasi					</div>				</td>			</tr>			<tr>				<td valign=top>					<div style =font-family:arial;font-size:12px;text-align:justify;color:#ffffff;line-height:18px;>Volume suara semua lagu telah dinormalisasi, sehingga antara satu lagu dengan lagu yang lain volume terdengar stabil.					</div>				</td>			</tr>			</table>		</td>	</tr>    </table></td></tr></table>');" onmouseout="tooltip.hide();"/>
+				</div>
+				<br><br>
+				</div>
+
+          </section>
+<!-- End Members Section -->
+ </main><!-- End #main -->
+
+
 @endsection
 
 @section('js-internal')
 <!-- add tag js in here -->
 
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
 @endsection
