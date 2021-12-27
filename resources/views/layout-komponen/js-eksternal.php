7afcbfe6a106ef2326b225ab831629ff67a32da6
raihanabdul13 <raihanabdul13@gmail.com>
2021-12-27 16:42:45 +0700

    update navbar revisi UI

--- resources/views/layout-komponen/js-eksternal.php
+++ resources/views/layout-komponen/js-eksternal.php
@@ -7,3 +7,11 @@
 <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
 <script src="{{ asset('/assets/js/vendor/popper.min.js') }}"></script>
 <script src="{{ asset('/dist/js/bootstrap.min.js') }}"></script>
+<script>
+    var viewMode = getCookie("view-mode");
+if(viewMode == "desktop"){
+    viewport.setAttribute('content', 'width=1024');
+}else if (viewMode == "mobile"){
+    viewport.setAttribute('content', 'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no');
+}
+</script>
--- resources/views/layout-komponen/navbar.blade.php
+++ resources/views/layout-komponen/navbar.blade.php
@@ -13,18 +13,16 @@
             <a href="" style="margin-left:5px;">ENG</a>
           </div>
           <div class="dropdown">
-            <a class="nav-link" style="" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <a class="nav-link"  href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <span class="navbar-toggler-icon"></span>
             </a>
             <div class="dropdown-menu" style="right: -20px; left: auto;background-color:#2d1923;" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="{{ url('/news') }}">News</a>
-              <a class="dropdown-item" href="{{ url('/lokasi') }}">Lokasi</a>
-              <a class="dropdown-item" href="{{ url('/daftar-lagu') }}">Daftar Lagu</a>
-              <a class="dropdown-item" href="{{ url('/profile') }}">Profil</a>
-              <a class="dropdown-item" href="{{ url('/waralaba') }}">Waralaba</a>
-              <a class="dropdown-item" href="{{ url('/karir') }}">Karir</a>
-              <a class="dropdown-item" href="{{ url('/FAQ') }}">FAQ</a>
-              <a class="dropdown-item" href="{{ url('/kontak') }}">Kontak</a>
+              <a class="dropdown-item" href="{{ url('/lokasi') }}">LOKASI</a>
+              <a class="dropdown-item" href="{{ url('/news') }}">BERITA</a>
+              <a class="dropdown-item" href="{{ url('/profile') }}">PROFILE</a>
+              <a class="dropdown-item" href="{{ url('/waralaba') }}">WARALABA</a>
+              <a class="dropdown-item" href="{{ url('/karir') }}">KARIR</a>
+              <a class="dropdown-item" href="{{ url('/FAQ') }}">KONTAK KAMI</a>
             </div>
           </div>
         </div>
@@ -32,26 +30,22 @@
       <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav me-auto mb-2 mb-md-0 justify-content-end">
           <li class="nav-item mx-1">
-            <a class="nav-link @yield('navbar-home')" aria-current="page" href="{{ url('/news') }}">News</a>
+            <a class="nav-link @yield('navbar-home')" aria-current="page" href="{{ url('/lokasi') }}">LOKASI</a>
           </li>
           <li class="nav-item mx-1">
-            <a class="nav-link @yield('navbar-home')" aria-current="page" href="{{ url('/lokasi') }}">Lokasi</a>
+            <a class="nav-link @yield('navbar-home')" aria-current="page" href="{{ url('/news') }}">BERITA</a>
           </li>
           <li class="nav-item mx-1">
-            <a class="nav-link @yield('navbar-home')" aria-current="page" href="{{ url('/daftar-lagu') }}">Daftar Lagu</a>
-          </li>
-          <li class="nav-item mx-1">
-            <a class="nav-link @yield('navbar-home')" aria-current="page" href="{{ url('/profile') }}">Profile</a>
+            <a class="nav-link @yield('navbar-home')" aria-current="page" href="{{ url('/profile') }}">PROFILE</a>
           </li>
           <li class="nav-item mx-1 dropdown">
-            <a class="nav-link" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <a class="nav-link" href="#" id="navbarDropdown" style="color:white" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <span class="navbar-toggler-icon"></span>
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown" style="background-color:#2d1923;">
-              <a class="dropdown-item" href="{{ url('/waralaba') }}">Waralaba</a>
-              <a class="dropdown-item" href="{{ url('/karir') }}">Karir</a>
-              <a class="dropdown-item" href="{{ url('/FAQ') }}">FAQ</a>
-              <a class="dropdown-item" href="{{ url('/kontak') }}">Kontak</a>
+              <a class="dropdown-item" href="{{ url('/waralaba') }}">WARALABA</a>
+              <a class="dropdown-item" href="{{ url('/karir') }}">KARIR</a>
+              <a class="dropdown-item" href="{{ url('/kontak') }}">KONTAK KAMI</a>
             </div>
           </li>
           <li class="nav-item mx-1">
