a416448dfa30a04bce07435952fd27fd0b1d5437
raihanabdul13 <raihanabdul13@gmail.com>
2021-12-30 17:01:17 +0700

    update home page UI finished

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -30,10 +30,177 @@
 }
 .top-artis{
   background-color:#FFF;
+  min-height: 200px;
 }
 .rounded-circle{
   border: 5px solid #FFF;
   margin-top:-37px;
+}
+.btn-search{
+  border-radius:30px;
+  width:100px;
+  font-size:8px;
+  margin-left:25px;
+  margin-right:25px;
+}
+.top-artis-img{
+  margin:32px;
+}
+.song-container{
+  background-color:#FFF;
+  min-height:600px;
+}
+.song-list{
+  background-color:#F4F6F7;
+  padding-left:80px;
+  padding-right:80px;
+}
+.card-song{
+  margin-top:-80px;
+}
+.img-artis-content{
+    margin-top:-70px
+}
+.card{
+  width:16rem;
+}
+.card-img-top{
+  height:250px;
+}
+.card-title{
+font-size:16px;
+font-weight:bold;
+}
+.card-text{
+font-size:12px;
+}
+#myBtn{display: none;}
+
+.news-container{
+  min-height:300px;
+  background-color:#221E40;
+  margin-top: -75px
+}
+.news-konten{
+  margin-top:-200px;
+}
+.card-news{
+  width: 16rem;
+  margin: 0 auto; /* Added */
+  float: none; /* Added */
+  margin-bottom: 10px;
+}
+.card-img-news{
+  height:150px;
+}
+.news-time{
+  margin-bottom:-10px;
+  font-size:12px;
+}
+.title-news{
+  font-size:16px;
+}
+.card-news-popular{
+  width: 32rem;
+  margin: 0 auto; /* Added */
+  float: none; /* Added */
+  margin-bottom: 10px;
+  margin-top:10px;
+}
+.news-popular-title{
+    color:#106ec3;
+    font-size:12px;
+    margin-top:-15px;
+    margin-bottom:75px;
+    font-weight:bold
+}
+@media (max-width: 900px) {
+  .card-song{
+  margin-top:10px;
+  }
+}
+@media (max-width: 576px) {
+  .btn-search{
+  border-radius:8px;
+  width:60px;
+  font-size:8px;
+  margin: 5px;
+  }
+  .top-artis-img{
+    margin:16px;
+  }
+  .img-artis-content{
+    margin-top:-50px
+  }
+  .song-list{
+  background-color:#F5F5F5;
+  padding-left:30px;
+  padding-right:30px;
+  }
+  .card{
+  width:8rem;
+  }
+  .card-body{
+  min-height:5rem;
+  }
+  .card-img-top{
+  height:125px;
+  }
+  .card-song{
+  margin-top:10px;
+  }
+  .card-title{
+  font-size:12px;
+  font-weight:bold;
+  }
+  .card-text{
+  font-size:8px;
+  }
+  #more {display: none;}
+  #more2 {display: none;}
+  #myBtn{
+    display: block;
+    border-radius:20px;
+    font-size:8px;
+    margin-top:10px;
+    background-color:#221E40;
+  }
+  .news-container{
+  min-height:300px;
+  background-color:#221E40;
+  margin-top: -100px;
+  }
+  .card-news{
+  width: 8rem;
+  margin: 0 auto; /* Added */
+  margin-bottom: 10px;
+  }
+  .card-img-news{
+    height:80px;
+  }
+  .news-time{
+    margin-bottom:-10px;
+  }
+  .card-news-popular{
+    width: 18rem;
+    height: 5rem;
+    margin: 0 auto; /* Added */
+    margin-bottom: 10px;
+    margin-top:10px;
+  }
+  .title-news{
+  font-size:12px;
+  }
+  .news-time{
+    font-size:8px;
+    margin-top: 24px;
+  }
+  .news-popular-title{
+    color:#221E40;
+    font-size:8px;
+    margin-top:-15px;
+    margin-bottom:18px;
+  }
 }
 </style>
 <link rel="stylesheet" href="{{asset('admin/plugins/select2/css/select2.min.css')}}">
@@ -48,7 +215,7 @@
 <!-- search location  -->
 <div class="container-fluid text-white search-kota">
     <center>
-        <h4 class="p-4">Cari Oulet Terdekat</h4>
+        <h4 class="p-4 font-weight-bold">CARI OUTLET TERDEKAT</h4>
     </center>
         <div class="row py-4" >
             <div class="col-lg-3"></div>
@@ -66,21 +233,21 @@
             </div>
             <div class="col-lg-3"></div>
         </div>
-        <div class="container text-center">
+        <div class="container-fluid text-center">
           <div class="row pt-3">
             <div class="col-lg-2"></div>
-            <?php for($i=0;$i<4;$i++){?>
-              <div class="col-lg-2">
-                <a href="" class="btn btn-primary" style="border-radius:30px;width:125px;font-size:10px">Surabaya</a>
-              </div>
-              <?php };?>
+            <div class="col-lg-8">
+              <?php for($i=0;$i<4;$i++){?>
+                  <a href="" class="btn btn-primary btn-search" style="">Surabaya</a>
+                <?php };?>
             </div>
             <div class="col-lg-2"></div>
           </div>
+        </div>
           <!-- /.row -->
           <!-- top artis title -->
         <div class="container text-center py-5">
-          <h4>Top Artis</h4>
+          <h4 class="font-weight-bold">TOP ARTIST</h4>
         </div>
         <!-- end top artis title -->
 </div>
@@ -90,35 +257,275 @@
   <div class="container text-center">
     <div class="row">
       <div class="col-lg-2"></div>
-      <div class="col-lg-2">
-      <img class="img-fluid rounded-circle" alt="75x75" width="75" height="75" src="https://mdbootstrap.com/img/Photos/Avatars/img%20(30).jpg"
-          data-holder-rendered="true">
-      </div>
-      <div class="col-lg-2">
-      <img class="img-fluid rounded-circle" alt="75x75" width="75" height="75" src="https://mdbootstrap.com/img/Photos/Avatars/img%20(30).jpg"
-          data-holder-rendered="true">
-      </div>
-      <div class="col-lg-2">
-      <img class="img-fluid rounded-circle" alt="75x75" width="75" height="75" src="https://mdbootstrap.com/img/Photos/Avatars/img%20(30).jpg"
-          data-holder-rendered="true">
-      </div>
-      <div class="col-lg-2">
-      <img class="img-fluid rounded-circle" alt="75x75" width="75" height="75" src="https://mdbootstrap.com/img/Photos/Avatars/img%20(30).jpg"
-          data-holder-rendered="true">
+      <div class="col-lg-8">
+        <div class="row img-artis-content" >
+          <div class="col-lg-12 col-md-12 col-sm-12">
+            <img class="img-fluid rounded-circle top-artis-img" alt="75x75" width="75" height="75" src="https://mdbootstrap.com/img/Photos/Avatars/img%20(30).jpg" data-holder-rendered="true">
+            <img class="img-fluid rounded-circle top-artis-img" alt="75x75" width="75" height="75" src="https://mdbootstrap.com/img/Photos/Avatars/img%20(30).jpg" data-holder-rendered="true">
+            <img class="img-fluid rounded-circle top-artis-img" alt="75x75" width="75" height="75" src="https://mdbootstrap.com/img/Photos/Avatars/img%20(30).jpg" data-holder-rendered="true">
+            <img class="img-fluid rounded-circle top-artis-img" alt="75x75" width="75" height="75" src="https://mdbootstrap.com/img/Photos/Avatars/img%20(30).jpg" data-holder-rendered="true">
+            <img class="img-fluid rounded-circle top-artis-img" alt="75x75" width="75" height="75" src="https://mdbootstrap.com/img/Photos/Avatars/img%20(30).jpg" data-holder-rendered="true">
+          </div>
+        </div>
       </div>
       <div class="col-lg-2"></div>
     </div>
   </div>
   <!-- end of Top artis -->
-  <div class="container">
-
-  </div>
 </div>
+<!-- song list -->
+<div class="container-fluid song-container">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-2"></div>
+      <div class="col-lg-8 song-list">
+        <div class="row card-song">
+          <div class="col-6">
+            <div class="row px-3">
+              <div class="col-lg-12 col-sm-12">
+                 <h6 style="text-transform: uppercase;padding-bottom:10px;color:#221E40">lagu terlaris</h6>
+                 <div class="card">
+                    <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(30).jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Judul Band</h5>
+                      <p class="card-text">Judul Lagu</p>
+                    </div>
+                 </div>
+                 <div class="card my-4">
+                   <div class="card-body">
+                     <div class="row">
+                       <div class="col-12 my-2">
+                         <h5 class="card-title">Judul Band</h5>
+                         <p class="card-text">Judul Lagu</p>
+                       </div>
+                       <div class="col-12 my-2">
+                         <h5 class="card-title">Judul Band</h5>
+                         <p class="card-text">Judul Lagu</p>
+                       </div>
+                       <div class="col-12 my-2">
+                         <h5 class="card-title">Judul Band</h5>
+                         <p class="card-text">Judul Lagu</p>
+                       </div>
+                       <span id="dots"></span>
+                       <span id="more">
+                       <div class="col-12 my-2">
+                         <h5 class="card-title">Judul Band</h5>
+                         <p class="card-text">Judul Lagu</p>
+                       </div>
+                       <div class="col-12 my-2">
+                         <h5 class="card-title">Judul Band</h5>
+                         <p class="card-text">Judul Lagu</p>
+                       </div>
+                       </span>
+                     </div>
+                     <center>
+                       <button href="{{url('/')}}" onclick="buttonLess()" id="myBtn" class="btn btn-primary">view all <i class="fas fa-arrow-alt-circle-right"></i></button>
+                     </center>
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </div>
+           <div class="col-6">
+             <div class="row px-3">
+               <div class="col-lg-12 col-sm-12">
+                 <h6 style="text-transform: uppercase; padding-bottom:10px;color:#221E40">lagu terbaru</h6>
+                 <div class="card">
+                    <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(30).jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Judul Band</h5>
+                      <p class="card-text">Judul Lagu</p>
+                    </div>
+                 </div>
+                  <div class="card my-4">
+                    <div class="card-body">
+                      <div class="row">
+                        <div class="col-12 my-2">
+                          <h5 class="card-title">Judul Band</h5>
+                          <p class="card-text">Judul Lagu</p>
+                        </div>
+                        <div class="col-12 my-2">
+                          <h5 class="card-title">Judul Band</h5>
+                          <p class="card-text">Judul Lagu</p>
+                        </div>
+                        <div class="col-12 my-2">
+                          <h5 class="card-title">Judul Band</h5>
+                          <p class="card-text">Judul Lagu</p>
+                        </div>
+                        <span id="dots2"></span>
+                        <span id="more2">
+                        <div class="col-12 my-2">
+                          <h5 class="card-title">Judul Band</h5>
+                          <p class="card-text">Judul Lagu</p>
+                        </div>
+                        <div class="col-12 my-2">
+                          <h5 class="card-title">Judul Band</h5>
+                          <p class="card-text">Judul Lagu</p>
+                        </div>
+                        </span>
+                      </div>
+                      <center>
+                        <button href="{{url('/')}}" onclick="buttonLess2()" id="myBtn" class="btn btn-primary">view all <i class="fas fa-arrow-alt-circle-right"></i></button>
+                      </center>
+                    </div>
+                  </div>
+                 </div>
+               </div>
+             </div>
+             
+          </div>
+        </div>
+      </div>
+      </div>
+      <div class="col-lg-2"></div>
+    </div>
+</div>
+<!-- end song list -->
+<!-- news -->
+<div class="container-fluid news-container">
+</div>
+<div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-2"></div>
+      <div class="col-lg-8">
+        <div class="container mx-2 news-konten">
+            <h4 class="text-white font-weight-bold d-inline-block mr-2">Lates News</h4> <a href="{{url('/news')}}">view all</a>
+            <div class="row">
+              <div class="col-6 my-2">
+                <div class="card card-news">
+                  <img src="https://img.buzzfeed.com/buzzfeed-static/static/2014-05/campaign_images/webdr08/1/1/25-crowd-pleasing-karaoke-songs-that-are-actually-2-25835-1398920438-12_dblbig.jpg" class="card-img-top card-img-news" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title title-news">Card title</h5>
+                    <p class="news-time"><?=now()?>
+                      <a href="" class="" style="float:right;color:black"><i class="fas fa-book-open"></i></a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-6 my-2">
+                <div class="card card-news">
+                  <img src="https://img.buzzfeed.com/buzzfeed-static/static/2014-05/campaign_images/webdr08/1/1/25-crowd-pleasing-karaoke-songs-that-are-actually-2-25835-1398920438-12_dblbig.jpg" class="card-img-top card-img-news" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title title-news">Card title</h5>
+                    <p class="news-time"><?=now()?>
+                      <a href="" class="" style="float:right;color:black"><i class="fas fa-book-open"></i></a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-12">
+                <div class="card card-news-popular">
+                <div class="row no-gutters">
+                  <div class="col-6">
+                    <img src="https://img.buzzfeed.com/buzzfeed-static/static/2014-05/campaign_images/webdr08/1/1/25-crowd-pleasing-karaoke-songs-that-are-actually-2-25835-1398920438-12_dblbig.jpg" class="card-img-top card-img-news" alt="...">
+                  </div>
+                  <div class="col-6">
+                    <div class="card-body text-bottom">
+                      <p class="news-popular-title">news</p>
+                      <h5 class="card-title">Card title</h5>
+                      <p class="news-time"><?=now()?>
+                        <a href="" class="" style="float:right;color:black"><i class="fas fa-book-open"></i></a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                </div>
+              </div>
+              <div class="col-12">
+                <div class="card card-news-popular">
+                <div class="row no-gutters">
+                  <div class="col-6">
+                    <img src="https://img.buzzfeed.com/buzzfeed-static/static/2014-05/campaign_images/webdr08/1/1/25-crowd-pleasing-karaoke-songs-that-are-actually-2-25835-1398920438-12_dblbig.jpg" class="card-img-top card-img-news" alt="...">
+                  </div>
+                  <div class="col-6">
+                    <div class="card-body text-bottom">
+                      <p class="news-popular-title">news</p>
+                      <h5 class="card-title">Card title</h5>
+                      <p class="news-time"><?=now()?>
+                        <a href="" class="" style="float:right;color:black"><i class="fas fa-book-open"></i></a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                </div>
+              </div>
+              <div class="col-12">
+                <div class="card card-news-popular">
+                <div class="row no-gutters">
+                  <div class="col-6">
+                    <img src="https://img.buzzfeed.com/buzzfeed-static/static/2014-05/campaign_images/webdr08/1/1/25-crowd-pleasing-karaoke-songs-that-are-actually-2-25835-1398920438-12_dblbig.jpg" class="card-img-top card-img-news" alt="...">
+                  </div>
+                  <div class="col-6">
+                    <div class="card-body text-bottom">
+                      <p class="news-popular-title">News</p>
+                      <h5 class="card-title">Card title</h5>
+                      <p class="news-time"><?=now()?>
+                        <a href="" class="" style="float:right;color:black"><i class="fas fa-book-open"></i></a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                </div>
+              </div>
+              <div class="col-12">
+                <div class="card card-news-popular">
+                <div class="row no-gutters">
+                  <div class="col-6">
+                    <img src="https://img.buzzfeed.com/buzzfeed-static/static/2014-05/campaign_images/webdr08/1/1/25-crowd-pleasing-karaoke-songs-that-are-actually-2-25835-1398920438-12_dblbig.jpg" class="card-img-top card-img-news" alt="...">
+                  </div>
+                  <div class="col-6">
+                    <div class="card-body text-bottom">
+                      <p class="news-popular-title">news</p>
+                      <h5 class="card-title">Card title</h5>
+                      <p class="news-time"><?=now()?>
+                        <a href="" class="" style="float:right;color:black"><i class="fas fa-book-open"></i></a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>
+      <div class="col-lg-2"></div>
+    </div>
+</div>
+<!-- end news -->
 @endsection
 
 @section('js-internal')
 <!-- add tag js in here -->
+<script>
+  function buttonLess(){
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
 
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      moreText.style.display = "inline";
+    }
+  }
+  function buttonLess2(){
+    var dots = document.getElementById("dots2");
+    var moreText = document.getElementById("more2");
+    var btnText = document.getElementById("myBtn");
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      moreText.style.display = "inline";
+    }
+  }
+</script>
 <!-- Select2  -->
 <script src="{{ asset('admin/plugins/select2/js/select2.full.min.js') }}"></script>
 <script>
