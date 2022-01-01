93850d86018a7eaa4bbd8ec64844721dd49cafef
salsabilananda <salsabilananda222@gmail.com>
2022-01-01 20:18:38 +0700

    updated migrasi2

--- database/migrations/2022_01_01_014529_create_waralabaregister_table.php
+++ database/migrations/2022_01_01_014529_create_waralabaregister_table.php
@@ -15,6 +15,47 @@ class CreateWaralabaregisterTable extends Migration
     {
         Schema::create('waralabaregister', function (Blueprint $table) {
             $table->id();
+            $table->string('name',50);
+            $table->boolean('gender');
+            $table->string('age',2);
+            $table->string('last_education',15);
+            $table->boolean('marital');
+            $table->string('city',64);
+            $table->string('province',32);
+            $table->string('official_address');
+            $table->string('home_phone',16);
+            $table->string('official_phone',16);
+            $table->string('mobile_phone',15);
+            $table->string('email',50);
+            $table->string('job',30);
+            $table->string('bussiness_field',50);
+            $table->text('other')->nullable();
+            $table->string('company_name',50);
+            $table->string('postion_company',30);
+            $table->text('other_2')->nullable();
+            $table->string('address_2');
+            $table->string('city_2',64);
+            $table->string('province_2',32);
+            $table->string('status_penguasaan',30);
+            $table->string('address_3');
+            $table->string('city_3',64);
+            $table->string('province_3',32);
+            $table->text('known_as_area');
+            $table->text('other_3')->nullable();
+            $table->integer('building_area');
+            $table->string('left_business_name',50);
+            $table->string('left_business_field',30);
+            $table->string('right_business_name',50);
+            $table->string('right_business_field',30);
+            $table->string('front_business_name',50);
+            $table->string('front_business_field',30);
+            $table->string('behind_business_name',50);
+            $table->string('behind_business_field',30);
+            $table->integer('front_road_width');
+            $table->string('front_road_traffic',30);
+            $table->boolean('confirm_completenes');
+            $table->boolean('confirm_register');
+            $table->boolean('status');
             $table->timestamps();
         });
     }
--- database/migrations/2022_01_01_014745_create_careerform_table.php
+++ database/migrations/2022_01_01_014745_create_careerform_table.php
@@ -15,6 +15,29 @@ class CreateCareerformTable extends Migration
     {
         Schema::create('careerform', function (Blueprint $table) {
             $table->id();
+            $table->string('name',50);
+            $table->string('formal_education',50);
+            $table->string('informal_education',50);
+            $table->string('jurusan',50);
+            $table->string('place_birth',50);
+            $table->date('date_birth');
+            $table->integer('height');
+            $table->integer('weight');
+            $table->boolean('gender');
+            $table->boolean('status_marital');
+            $table->string('phone',15);
+            $table->string('mobile_phone',15);
+            $table->string('email',50);
+            $table->string('address');
+            $table->string('no_ktp',16);
+            $table->integer('expected_salary');
+            $table->string('language',50);
+            $table->string('instrument_music',50);
+            $table->string('computer',50);
+            $table->text('other_expertise');
+            $table->string('cv');
+            $table->string('ijazah');
+            $table->string('linkedin');
             $table->timestamps();
         });
     }
