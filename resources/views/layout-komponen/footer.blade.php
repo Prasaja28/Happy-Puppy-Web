c713855bcb53ba321f25ffa4c9f54fdadcf595c4
raihanabdul13 <raihanabdul13@gmail.com>
2022-01-08 15:41:23 +0700

    Update footer.blade.php

--- resources/views/layout-komponen/footer.blade.php
+++ resources/views/layout-komponen/footer.blade.php
@@ -2,33 +2,90 @@
 	<div class="row m-0 text-white" style="background-color: #000000;">
     <div class="container">
       <div class="row">
-        <div class="col-md-6 py-2 d-flex">
-         <img src="{{ asset('/img/HappupLogo.png') }}" width="150" height="50" alt="Logo">
-         <p  style="font-size: 10px; padding-left :10px"> PT IMPERIUM HAPPY PUPPY <br> JL HR Muhammad 73B Surabaya 80226 <br> Telp (031) 734-338, Fax. (031) 734-2911 <br> E-mail : infohappy-puppy.com</p>
+        <div class="col-6 py-2 d-flex">
+          <div class="row">
+            <div class="col-sm-12 pt-3">
+              <img src="{{ asset('/img/HappupLogo.png') }}" class="img-logo" alt="Logo">
+              <img src="{{ asset('/img/HappupLogo.png') }}" class="img-logo" alt="Logo">
+              <img src="{{ asset('/img/HappupLogo.png') }}" class="img-logo" alt="Logo">
+              <img src="{{ asset('/img/HappupLogo.png') }}" class="img-logo" alt="Logo">
+              <img src="{{ asset('/img/HappupLogo.png') }}" class="img-logo" alt="Logo">
+            </div>
+          </div>
+         
         </div>
-
-        <div class="col-md-6 py-2">
-          <div class="text-center">
-              <a class="btn btn-primary" style="width:150px;font-size:10px" role="button" href="">Become a member</a>
-            <a class="m-1" style="display: inline-block; color:white;" title="Facebook" target="_blank" href="https://www.facebook.com/helloworld">
-              <i class="fab fa-facebook-f fa-lg"></i>
-            </a>
-            <a class="m-1" style="display: inline-block; color:white;" title="Twitter" target="_blank" href="https://www.twitter.com/helloworld">
-              <i class="fab fa-twitter fa-lg"></i>
-            </a>
-            <a class="m-1" style="display: inline-block; color:white;" title="Instagram" target="_blank" href="https://www.twitter.com/helloworld">
-              <i class="fab fa-instagram fa-lg"></i>
-            </a>
-            <a class="m-1" style="display: inline-block; color:white;" title="Instagram" target="_blank" href="https://www.twitter.com/helloworld">
-              <i class="fab fa-apple fa-lg"></i>
-            </a>
-            <a class="m-1" style="display: inline-block; color:white;" title="Instagram" target="_blank" href="https://www.twitter.com/helloworld">
-              <i class="fab fa-google-play fa-lg"></i>
-              <p style="font-size: 10px; padding-left :10px; margin-top:10px"><a href="">Syarat & ketentuan</a> | &copy; 2011 PT IMPERIUM HAPPY PUPPY,All Rights Reserved</p>
-            </a>
+        <div class="col-6 py-2">
+          <div class="text-right">
+            <div class="row">
+              <div class="col-12 pt-2">
+                <a class="m-2" style="display: inline-block; color:white;" title="Facebook" target="_blank" href="https://www.facebook.com/helloworld">
+                  <i class="fab fa-facebook-f fa-sm"></i>
+                </a>
+                <a class="m-2" style="display: inline-block; color:white;" title="Twitter" target="_blank" href="https://www.twitter.com/helloworld">
+                  <i class="fab fa-twitter fa-sm"></i>
+                </a>
+                <a class="m-2" style="display: inline-block; color:white;" title="Instagram" target="_blank" href="https://www.twitter.com/helloworld">
+                  <i class="fab fa-instagram fa-sm"></i>
+                </a>
+                <a class="m-2" style="display: inline-block; color:white;" title="Instagram" target="_blank" href="https://www.twitter.com/helloworld">
+                  <i class="fab fa-apple fa-sm"></i>
+                </a>
+                <a class="m-2" style="display: inline-block; color:white;" title="Instagram" target="_blank" href="https://www.twitter.com/helloworld">
+                  <i class="fab fa-google-play fa-sm"></i>
+                </a>
+              </div>
+              <div class="col-12">
+                <a class="btn btn-primary" id="become-member" style="width:150px;font-size:10px" role="button" href="">Become a member</a>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+        <div class="col-6">
+          <div class="row">
+            <div class="col-sm-6 instansi-kontak">
+              <p> PT IMPERIUM HAPPY PUPPY <br> JL HR Muhammad 73B Surabaya 80226</p> 
+            </div>
+            <div class="col-sm-6 instansi-kontak">
+              <p> Telp (031) 734-338, Fax. (031) 734-2911 <br> E-mail : infohappy-puppy.com</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-6 text-right">
+          <div class="row">
+            <div class="col-12 pt-3">
+              <a href="/" id="language-mobile">INA</a>
+              <a id="language-mobile">|</a>
+              <a href="/" id="language-mobile">ENG</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 text-right">
+          <p style="font-size: 10px; padding-left :10px;"><a href="">Syarat & ketentuan</a> | &copy; 2011 PT IMPERIUM HAPPY PUPPY,All Rights Reserved</p>
+        </div>
     </div>
 	</div>
 </footer>
+<style>
+.instansi-kontak{
+  font-size: 10px; 
+  padding-left :10px
+}
+.img-logo{
+  width: 75px;
+  height: 25px;
+}
+@media (max-width: 576px) {
+  .instansi-kontak{
+    font-size: 8px;
+    padding-left :10px;
+    margin-top:-10px;
+  }
+  .img-logo{
+    width:50px;
+    height:20px;
+  }
+  #become-member{
+    margin-bottom:-40px;
+  }
+}
+</style>
