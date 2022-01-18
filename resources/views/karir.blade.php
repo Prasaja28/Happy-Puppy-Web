d2e43713e3d460af6efc836eb237a5d0b03d32a8
salsabilananda <salsabilananda222@gmail.com>
2022-01-19 02:55:31 +0700

    font

--- resources/views/karir.blade.php
+++ resources/views/karir.blade.php
@@ -13,10 +13,10 @@
   -moz-background-size: cover;
   -o-background-size: cover;
   background-size: cover;
-  background-position: rightl;
+  background-position: right;
   background-image: url('/img/karir.png')
-
 }
+
 #txt2{
     color: white;
     position: absolute;
@@ -27,6 +27,7 @@
     min-height: 550px;
     text-indent: 1in;
 }
+
 #txt1{
     color: white;
     position: absolute;
@@ -38,289 +39,6 @@
     min-height: 500px;
     text-indent: 1in;
 }
-
-.header{
-  min-height: 18vh;
-  background: no-repeat center scroll;
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
-  background-position: rightl;
-  background-color: #1f1f1f;
-}
-.rounded-circle{
-  border: 5px solid #FFF;
-  margin-top:-37px;
-}
-
-.l1{
-  width: 135px;
-  height: 135px;
-  background: #FA8072;
-  border-radius: 100%;
-  border: 5px solid #FFF;
-  margin-top:-37px;
-}
-
-.l2{
-  width: 135px;
-  height: 135px;
-  background: #87CEFA;
-  border-radius: 100%;
-  border: 5px solid #FFF;
-  margin-top:-37px;
-}
-
-.l3{
-  width: 135px;
-  height: 135px;
-  background: #ADFF2F;
-  border-radius: 100%;
-  border: 5px solid #FFF;
-  margin-top:-37px;
-}
-
-.l4{
-  width: 135px;
-  height: 135px;
-  background: grey;
-  border-radius: 100%;
-  border: 5px solid #FFF;
-  margin-top:-37px;
-}
-
-.l5{
-  width: 135px;
-  height: 135px;
-  background: #FFD700;
-  border-radius: 100%;
-  border: 5px solid #FFF;
-  margin-top:-37px;
-}
-
-.h1 {
-  color: #ffffff;
-  font-size:25px;
-  text-indent: 1.2in;
-}
-
-.h1 {
-  color: #ffffff;
-  font-size:25px;
-}
-
-.h2 {
-  color: #ffffff;
-  font-size:15px;
-  font-style: italic;
-  text-indent: 1.2in;
-}
-
-.h3 {
-  color: #000000;
-  font-size:15px;
-}
-.h4 {
-  color: #808080;
-  font-size:15px;
-  position: absolute;
-    width: 400px;
-    height: 200px;
-padding-top: 0px;
-padding-right: 30px;
-padding-bottom: 10px;
-padding-left: 60px;
-}
-
-.h5 {
-  color: #808080;
-  font-size:15px;
-  position: absolute;
-    width: 600px;
-    height: 200px;
-padding-top: 0px;
-padding-right: 60px;
-padding-bottom: 0px;
-padding-left: 240px;
-}
-
-}
-.ratakiri { text-align: left; }
-.ratakanan { text-align: right; }
-.ratatengah { text-align: center; }
-.ratakirikanan { 
-  text-align: justify;
-   }
-
-.center {
-    margin: auto;
-    margin-top: 50px;
-    width: 82%;
-    padding: 15px;
-    color: black;
-    background-color:#D3D3D3;
-    text-indent: 0.4in;
-}
-.bold { font-weight: bold; 
-  font-size:20px;}
-  .img-artis-content{
-    margin-top:-50px
-
-  }
-
-.border {
-  border-width: 10px;
-  border-style: solid;
-  border-top-color: red;
-  border-bottom-color: blue;
-  border-left-color: green;
-  border-right-color: yellow;
-  }
-
-.card-body{
-  min-height:5rem;
-  text-align: right;
-  width: 30%;
-  }
-.vl {
-  border-left: 6px solid white;
-  min-height: 6vh;
-  position: absolute;
-  left: 50%;
-  margin-left: -3px;
-  top: 5;
-}
-@media (max-width: 576px) {
-#txt2{
-    color: white;
-    position: absolute;
-    bottom: 0;
-    text-align: justify;
-    margin: 0;
-    font-size: 25px;
-    min-height: 550px;
-    text-indent: 0.5in;
-}
-#txt1{
-    color: white;
-    position: absolute;
-    bottom: 0;
-    text-align: justify;
-    margin: 0;
-    font-size: 25px;
-    font-weight: bold;
-    min-height: 500px;
-    text-indent: 0.5in;
-}
-
-.header{
-  min-height: 18vh;
-  background: no-repeat center scroll;
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
-  background-position: rightl;
-  background-color: #1f1f1f;
-}
-.rounded-circle{
-  border: 5px solid #FFF;
-  margin-top:-37px;
-}
-
-.l1{
-  width: 90px;
-  height: 90px;
-  background: #FA8072;
-  border-radius: 100%;
-  border: 5px solid #FFF;
-  margin-top:-37px;
-}
-
-.l2{
-  width: 90px;
-  height: 90px;
-  background: #87CEFA;
-  border-radius: 100%;
-  border: 5px solid #FFF;
-  margin-top:-37px;
-}
-
-.l3{
-  width: 90px;
-  height: 90px;
-  background: #ADFF2F;
-  border-radius: 100%;
-  border: 5px solid #FFF;
-  margin-top:-37px;
-}
-
-.l4{
-  width: 90px;
-  height: 90px;
-  background: grey;
-  border-radius: 100%;
-  border: 5px solid #FFF;
-  margin-top:-37px;
-}
-
-.l5{
-  width: 90px;
-  height: 90px;
-  background: #FFD700;
-  border-radius: 100%;
-  border: 5px solid #FFF;
-  margin-top:-37px;
-}
-
-.h1 {
-  color: #ffffff;
-  font-size:15px;
-}
-
-.h2 {
-  color: #ffffff;
-  font-size:10px;
-  font-style: italic;
-  text-indent: 0.5in;
-}
-
-.h3 {
-  color: #000000;
-  font-size:15px;
-}
-.h4 {
-  color: #808080;
-  font-size:9px;
-  position: absolute;
-    width: 120px;
-    height: 60px;
-padding-top: 0px;
-padding-right: 5px;
-padding-bottom: 10px;
-padding-left: 5px;
-}
-
-.h5 {
-  color: #808080;
-  font-size:9px;
-  position: absolute;
-    width: 250px;
-    height: 60px;
-padding-top: 0px;
-padding-right: 0px;
-padding-bottom: 0px;
-padding-left: 130px;
-}
-
-}
-.ratakiri { text-align: left; }
-.ratakanan { text-align: right; }
-.ratatengah { text-align: center; }
-.ratakirikanan { 
-  text-align: justify;
-   }
 
 .center {
     margin: auto;
@@ -332,206 +50,15 @@ padding-left: 130px;
     background-color:#D3D3D3;
     text-indent: 0.1in;
 }
-.bold { font-weight: bold; 
-  font-size:15px;}
-  .img-artis-content{
-    margin-top:-50px
 
-  }
-
-.border {
-  border-width: 10px;
-  border-style: solid;
-  border-top-color: red;
-  border-bottom-color: blue;
-  border-left-color: green;
-  border-right-color: yellow;
-  }
-
-.card-body{
-  min-height:5rem;
-  text-align: right;
-  width: 30%;
-  }
-
-.vl {
-  border-left: 6px solid white;
-  min-height: 6vh;
-  position: absolute;
-  left: 50%;
-  margin-left: -3px;
-  top: 5;
-}
-@media (max-width: 576px) {
-#txt2{
-    color: white;
-    position: absolute;
-    bottom: 0;
-    text-align: justify;
-    margin: 0;
-    font-size: 25px;
-    min-height: 550px;
-    text-indent: 0.5in;
-}
-#txt1{
-    color: white;
-    position: absolute;
-    bottom: 0;
-    text-align: justify;
-    margin: 0;
-    font-size: 25px;
-    font-weight: bold;
-    min-height: 500px;
-    text-indent: 0.5in;
-}
-
-.header{
-  min-height: 18vh;
-  background: no-repeat center scroll;
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
-  background-position: rightl;
-  background-color: #1f1f1f;
-}
-.rounded-circle{
-  border: 5px solid #FFF;
-  margin-top:-37px;
-}
-
-.l1{
-  width: 90px;
-  height: 90px;
-  background: #FA8072;
-  border-radius: 100%;
-  border: 5px solid #FFF;
-  margin-top:-37px;
-}
-
-.l2{
-  width: 90px;
-  height: 90px;
-  background: #87CEFA;
-  border-radius: 100%;
-  border: 5px solid #FFF;
-  margin-top:-37px;
-}
-
-.l3{
-  width: 90px;
-  height: 90px;
-  background: #ADFF2F;
-  border-radius: 100%;
-  border: 5px solid #FFF;
-  margin-top:-37px;
-}
-
-.l4{
-  width: 90px;
-  height: 90px;
-  background: grey;
-  border-radius: 100%;
-  border: 5px solid #FFF;
-  margin-top:-37px;
-}
-
-.l5{
-  width: 90px;
-  height: 90px;
-  background: #FFD700;
-  border-radius: 100%;
-  border: 5px solid #FFF;
-  margin-top:-37px;
-}
-
-.h11 {
-  color: #ffffff;
-  font-size:15px;
-  position: absolute;
-      width: 70px;
-    height: 0px;
-padding-top: 0px;
-padding-right: 5px;
-padding-bottom: 30px;
-padding-left: 5px;
-
-}
-.h1 {
-  color: #ffffff;
-  font-size:15px;
-  text-indent: 0.5in;
-
-
-}
-
-.h2 {
-  color: #ffffff;
-  font-size:10px;
-  font-style: italic;
-  text-indent: 0.5in;
-}
-
-.h3 {
-  color: #000000;
-  font-size:15px;
-}
-.h4 {
-  color: #808080;
-  font-size:9px;
-  position: absolute;
-    width: 120px;
-    height: 60px;
-padding-top: 0px;
-padding-right: 5px;
-padding-bottom: 10px;
-padding-left: 5px;
-}
-
-.h5 {
-  color: #808080;
-  font-size:9px;
-  position: absolute;
-    width: 250px;
-    height: 60px;
-padding-top: 0px;
-padding-right: 0px;
-padding-bottom: 0px;
-padding-left: 130px;
-}
-
-}
-.ratakiri { text-align: left; }
-.ratakanan { text-align: right; }
-.ratatengah { text-align: center; }
-.ratakirikanan { 
-  text-align: justify;
-   }
-
-.center {
-    margin: auto;
-    margin-top: 50px;
-    margin-bottom: 50px;
-    width: 100%;
-    padding: 15px;
-    color: black;
-    background-color:#D3D3D3;
-    text-indent: 0.1in;
-}
-.bold { font-weight: bold; 
-  font-size:15px;}
-  .img-artis-content{
-    margin-top:-50px
-
-  }
-
-  .lowongan {
+.lowongan {
     margin: auto;
     margin-top: 50px;
     width: 100%;
     padding: 50px;
     color: black;
     background-color:#221E40;
+    font-family: 'Poppins';
 }
 
 h5 {
@@ -539,44 +66,34 @@ h5 {
   font-size:25px;
 }
 
-.border {
-  border-width: 10px;
-  border-style: solid;
-  border-top-color: red;
-  border-bottom-color: blue;
-  border-left-color: green;
-  border-right-color: yellow;
-  }
-
-.card-body{
+.card-body {
   min-height:5rem;
   text-align: right;
   width: 30%;
   }
 
+.container {
+  font-family: 'Poppins';
+}
+
 .content {
     margin: 5px 100px 0px 250px;
     width: 80%;
     padding: 30px;
+    font-family: 'Poppins';
 }
 
 .card {
     margin: 5px 100px 0px 275px;
     padding: 0px;
+    font-family: 'Poppins';
 }
 
-.vl {
-  border-left: 6px solid white;
-  min-height: 6vh;
-  position: absolute;
-  left: 50%;
-  margin-left: -3px;
-  top: 5;
-}
 
-.btn{
+.btn {
     color: white;
     background-color: #221E40;
+    font-family: 'Poppins';
 }
 
 
