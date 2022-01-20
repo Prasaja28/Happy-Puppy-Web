e841f6530e82f818d67a8099739fddd6f967890c
bamiwn <bambang.awan-2019@vokasi.unair.ac.id>
2022-01-20 12:31:21 +0700

    bambang: footer fresh + footer.css + icon footer home

--- resources/views/layout-komponen/footer.blade.php
+++ resources/views/layout-komponen/footer.blade.php
@@ -1,90 +1,89 @@
+{{-- FRESH FOOTER WITH RESPONSIVENESS --}}
+
+@section('css-internal')
+    <link rel="stylesheet" href="{{ asset('assets/css/footer.css') }}">
+
 <footer>
-	<div class="row m-0 text-white" style="background-color: #171730;">
-    <div class="container">
-      <div class="row">
-        <div class="col-6 py-2 d-flex">
-          <div class="row">
-            <div class="col-sm-12 pt-3">
-              <img src="{{ asset('/img/HappupLogo.png') }}" class="img-logo mr-4" alt="Logo">
-              <img src="{{ asset('/img/logofot1.png') }}" class="img-logo mr-4" alt="Logo">
-              <img src="{{ asset('/img/logofot2.png') }}" class="img-logo mr-4" alt="Logo">
-              <img src="{{ asset('/img/logofot3.png') }}" class="img-logo mr-4" alt="Logo">
-              <img src="{{ asset('/img/logofot4.png') }}" class="img-logo mr-4" alt="Logo">
-            </div>
-          </div>
+  <div class="container p-5">
+    <div class="row">
+      <div name="detail-info" class="col-6">
+        <div class="row justify-content-start">
+          <img src="{{ asset('/img/HappupLogo.png') }}" class="mr-2 mr-md-4">
+          <img src="{{ asset('/img/logofot1.png') }}" class="mr-2 mr-md-4">
+          <img src="{{ asset('/img/logofot3.png') }}" class="mr-2 mr-md-4">
+          <img src="{{ asset('/img/logofot2.png') }}" class="mr-2 mr-md-4 mt-4 mt-md-0">
+          <img src="{{ asset('/img/logofot4.png') }}" class="mt-4 mt-md-0">
         </div>
-        <div class="col-6 py-2">
-          <div class="text-right">
-            <div class="row">
-              <div class="col-12 pt-2">
-                <a class="m-2" style="display: inline-block; color:white;" title="Facebook" target="_blank" href="https://www.facebook.com/helloworld">
-                  <i class="fab fa-facebook-f fa-sm"></i>
-                </a>
-                <a class="m-2" style="display: inline-block; color:white;" title="Twitter" target="_blank" href="https://www.twitter.com/helloworld">
-                  <i class="fab fa-twitter fa-sm"></i>
-                </a>
-                <a class="m-2" style="display: inline-block; color:white;" title="Instagram" target="_blank" href="https://www.twitter.com/helloworld">
-                  <i class="fab fa-instagram fa-sm"></i>
-                </a>
-                <a class="m-2" style="display: inline-block; color:white;" title="Instagram" target="_blank" href="https://www.twitter.com/helloworld">
-                  <i class="fab fa-apple fa-sm"></i>
-                </a>
-                <a class="m-2" style="display: inline-block; color:white;" title="Instagram" target="_blank" href="https://www.twitter.com/helloworld">
-                  <i class="fab fa-google-play fa-sm"></i>
-                </a>
-              </div>
-              <div class="col-12">
-                <a class="btn btn-primary" id="become-member" style="width:150px;font-size:10px" role="button" href="">Become a member</a>
-              </div>
-            </div>
-          </div>
+      </div>
+
+      <div name="row-social-media" class="col-6">
+        <div class="row justify-content-end">
+          <a href="http://facebook.com">
+            <img class="mr-4 mr-md-5" src="{{ asset('img/icons/facebook.png') }}">
+          </a>
+
+          <a href="http://twitter.com">
+            <img class="mr-4 mr-md-5" src="{{ asset('img/icons/twitter.png') }}">
+          </a>
+
+          <a href="http://instagram.com">
+            <img class="mr-md-5" src="{{ asset('img/icons/instagram.png') }}">
+          </a>
+
+            <div name="border-right"></div>
+
+          <a href="http://apple.com">
+            <img class="mt-4 mt-md-0 mr-4 mr-md-5 pl-5 pl-md-0 ml-md-5" src="{{ asset('img/icons/apple.png') }}">
+          </a>
+          
+          <a href="http://google.com">
+            <img class="mt-4 mt-md-0" src="{{ asset('img/icons/google-play.png') }}">
+          </a>
         </div>
-        <div class="col-6">
-          <div class="row">
-            <div class="col-sm-6 instansi-kontak" style="font-family: 'Poppins';">
-              <p> PT IMPERIUM HAPPY PUPPY <br> JL HR Muhammad 73B Surabaya 80226</p> 
-            </div>
-            <div class="col-sm-6 instansi-kontak" style="font-family: 'Poppins';">
-              <p> Telp (031) 734-338, Fax. (031) 734-2911 <br> E-mail : infohappy-puppy.com</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-6 text-right">
-          <div class="row">
-            <div class="col-12 pt-3" style="font-family: 'Poppins';">
-              <a href="/" id="language-mobile">INA</a>
-              <a id="language-mobile">|</a>
-              <a href="/" id="language-mobile">ENG</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-12 text-right">
-          <p style="font-size: 10px; padding-left :10px;font-family: 'Poppins';"><a href="">Syarat & ketentuan</a> | &copy; 2011 PT IMPERIUM HAPPY PUPPY,All Rights Reserved</p>
-        </div>
+      </div>
     </div>
-	</div>
+
+    <div class="row mt-5">
+      <div class="col-6">
+        <div name="industry-info">
+          <div class="row justify-content-start">
+            <div class="col-12 col-md-6">
+              <p class="font-weight-bold">PT IMPERIUM HAPPY PUPPY</p>
+            </div>
+            
+            <div class="col-12 col-md-6">
+              <p class="font-weight-bold">Telp.(031) 734-3388, Fax.(031) 734-2911</p>
+            </div>
+          </div>
+
+          <div class="row justify-content-start">
+            <div class="col-12 col-md-6">
+              <p class="font-weight-bold">JL. HR Muhammad 73B Surabaya 60226</p>
+            </div>
+            
+            <div class="col-12 col-md-6">
+              <p class="font-weight-bold">E-mail: info@happy-puppy.com</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-6">
+        <div name="more-detail">
+          <div class="row justify-content-end">
+            <a href="#" class="btn btn-member rounded-0 text-white font-weight-bold">Become a member</a>
+          </div>
+
+          <div name="change-language" class="row justify-content-end my-5 my-md-3">
+            <a href="#">INA |</a>
+            <a href="#" class="pl-1">ENG</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div name="term-and-condition" class="row justify-content-md-end">
+      <p>Syarat & Ketentuan | @ 2011 PT Imperium Happy Puppy. All Right Reserved.</p>
+    </div>
+  </div>
 </footer>
-<style>
-.instansi-kontak{
-  font-size: 10px; 
-  padding-left :10px
-}
-.img-logo{
-  width: 75px;
-  height: 25px;
-}
-@media (max-width: 576px) {
-  .instansi-kontak{
-    font-size: 8px;
-    padding-left :10px;
-    margin-top:-10px;
-  }
-  .img-logo{
-    width:50px;
-    height:20px;
-  }
-  #become-member{
-    margin-bottom:-40px;
-  }
-}
-</style>
