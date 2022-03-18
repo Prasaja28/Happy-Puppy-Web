daea107408b5569708fd5e0ee252c8a8d160bc1f
Lathif21 <63155619+Lathif21@users.noreply.github.com>
2022-03-18 16:40:01 +0700

    add Career Model

---
+++ app/Models/Career.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Career extends Model
+{
+    use HasFactory;
+
+    protected $table = 'careerform';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'name',
+        'formal_education', 'informal_education', 'jurusan',
+        'place_birth', 'date_birt', 'height', 'weight',
+        'gender', 'status_marital', 'phone', 'mobile_phone',
+        'email', 'address', 'no_ktp', 'expected_salary', 'language',
+        'instrument_music', 'computer', 'other_expertise', 'cv', 'ijazah',
+        'linkedin', 'created_at', 'updated_at'
+    ];
+    public function users()
+    {
+        return $this->belongsTo(Jobs::class, 'jobvacancy_id');
+    }
+}
