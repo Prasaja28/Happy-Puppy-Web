dff939831b748747639c9855b5dbde32e58fe7cd
Lathif21 <63155619+Lathif21@users.noreply.github.com>
2022-03-18 16:37:14 +0700

    Tambah Jobs Controller

---
+++ app/Http/Controllers/JobsController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Career;
+use App\Models\Jobs;
+use Illuminate\Http\Request;
+
+class JobsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $jobs = Jobs::all();
+        return view('karir', compact('jobs'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function filter(Request $request)
+    {
+        $jobs = Jobs::select('*')
+            ->where([
+                ['name_job', 'like', '%' . $request->nama_job . '%'],
+                ['location', 'like', '%' . $request->lokasi . '%']
+            ])
+            ->get();
+        return view('karir', compact('jobs'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Jobs  $jobs
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Jobs $jobs)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Jobs  $jobs
+     * @return \Illuminate\Http\Response
+     */
+
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Jobs  $jobs
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Jobs $jobs)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Jobs  $jobs
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Jobs $jobs)
+    {
+        //
+    }
+}
