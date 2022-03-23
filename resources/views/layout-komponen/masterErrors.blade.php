05b12ace8b182a5c4636c860a8d547a57d63820d
Lathif21 <63155619+Lathif21@users.noreply.github.com>
2022-03-23 15:23:15 +0700

    add files master layout for errors

---
+++ resources/views/layout-komponen/masterErrors.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    @include('layout-komponen.css-eksternal')
+    <title>@yield('title')</title>
+    @yield('css-internal')
+
+    <script src="https://kit.fontawesome.com/34f8c2075a.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="{{ asset('assets/fontawesome/css/all.css') }}">
+</head>
+
+<body>
+    {{-- @include('layout-komponen.navbar') --}}
+    @include('layout-komponen.layout-konten')
+    @include('layout-komponen.footer')
+    @include('layout-komponen.js-eksternal')
+    @yield('js-internal')
+</body>
+
+</html>
