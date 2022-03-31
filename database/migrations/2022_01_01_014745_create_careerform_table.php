840ead78eaf909fc592a8281b5667dae56d1b744
Lathif21 <63155619+Lathif21@users.noreply.github.com>
2022-04-01 06:05:25 +0700

    update new key

--- database/migrations/2022_01_01_014745_create_careerform_table.php
+++ database/migrations/2022_01_01_014745_create_careerform_table.php
@@ -15,29 +15,26 @@ class CreateCareerformTable extends Migration
     {
         Schema::create('careerform', function (Blueprint $table) {
             $table->id();
-            $table->string('name',50);
-            $table->string('formal_education',50);
-            $table->string('informal_education',50);
-            $table->string('jurusan',50);
-            $table->string('place_birth',50);
+            $table->string('name', 50);
+            $table->string('formal_education', 50);
+            $table->string('place_birth', 50);
             $table->date('date_birth');
             $table->integer('height');
             $table->integer('weight');
             $table->boolean('gender');
             $table->boolean('status_marital');
-            $table->string('phone',15);
-            $table->string('mobile_phone',15);
-            $table->string('email',50);
+            $table->string('phone', 20)->nullable();
+            $table->string('mobile_phone', 20);
+            $table->string('email', 50);
             $table->string('address');
-            $table->string('no_ktp',16);
+            $table->string('no_ktp', 16);
             $table->integer('expected_salary');
-            $table->string('language',50);
-            $table->string('instrument_music',50);
-            $table->string('computer',50);
-            $table->text('other_expertise');
+            $table->string('language', 50)->nullable();
+            $table->string('instrument_music', 50)->nullable();
+            $table->string('computer', 50)->nullable();
             $table->string('cv');
             $table->string('ijazah');
-            $table->string('linkedin');
+            $table->string('linkedin')->nullable();
             $table->timestamps();
         });
     }
