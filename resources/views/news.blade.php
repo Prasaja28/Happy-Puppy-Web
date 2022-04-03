0b1205cf9c607284705ed47897cddaaa521b1329
Lathif21 <63155619+Lathif21@users.noreply.github.com>
2022-04-03 08:55:56 +0700

    connect to news detail

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -16,10 +16,12 @@
             background-position: rightl;
             background-image: url('/img/news.png')
         }
-        .header p{
+
+        .header p {
             font-weight: bold;
             margin-top: -65px;
         }
+
         #n1 {
             font-family: 'Poppins';
         }
@@ -245,6 +247,7 @@
             background-color: #221E40;
             /* font-family: 'Poppins'; */
         }
+
         #myBtn2 {
             position: absolute;
             display: block;
@@ -272,7 +275,8 @@
                 background-position: rightl;
                 background-image: url('/img/news.png')
             }
-            .header p{
+
+            .header p {
                 font-weight: bold;
                 font-size: 20px;
                 margin-top: -160px;
@@ -467,6 +471,7 @@
                 bottom: -3px;
                 left: 40%;
             }
+
             #myBtn2 {
                 display: block;
                 border-radius: 50px;
@@ -522,12 +527,15 @@
                     <div class="card card-news" style="border: none;z-index: 3;">
                         @if ($news_terbaru->count() != null)
                             <!-- <img src="/img/egypt.png" class="card-img-top card-img-news" alt="..."> -->
-                            <img src="{{$news_terbaru[0]->thumbnail}}" class="card-img-top card-img-news" alt="...">
+                            <img src="{{ $news_terbaru[0]->thumbnail }}" class="card-img-top card-img-news" alt="...">
                             <div class="card-body">
                                 <!-- <h5 class="card-title title-news" style="font-weight: bold;">Midnight In Egypt</h5> -->
-                                <h5 class="card-title title-news" style="font-weight: bold;">{{$news_terbaru[0]->news_title_id}}</h5>
+                                <h5 class="card-title title-news" style="font-weight: bold;">
+                                    {{ $news_terbaru[0]->news_title_id }}</h5>
                                 <p class="news-time"><?= date('M d, Y', strtotime($news_terbaru[0]->news_date)) ?>
-                                    <a href="" class="" style="float:right;color:black"><i class="fas fa-book-open"></i></a>
+                                    <a href="{{ route('detailNews', ['id' => $news_terbaru[0]->id]) }}"
+                                        class="" style="float:right;color:black"><i
+                                            class="fas fa-book-open"></i></a>
                                 </p>
                             </div>
                         @endif
@@ -535,11 +543,12 @@
                     <div class="card card-news1" style="border: none;z-index: 2;">
                         @if ($news_terbaru->count() != null)
                             <!-- <img src="/img/egypt.png" class="card-img-top card-img-news1" alt="..."> -->
-                            <img src="{{$news_terbaru[0]->thumbnail}}" class="card-img-top card-img-news1" alt="...">
+                            <img src="{{ $news_terbaru[0]->thumbnail }}" class="card-img-top card-img-news1" alt="...">
                         @endif
                         <div class="card-body">
                             <p class="news-time"><?= date('M d, Y') ?>
-                                <a href="" class="" style="float:right;color:black"><i class="fas fa-book-open"></i></a>
+                                <a href="{{ route('detailNews', ['id' => $news_terbaru[0]->id]) }}" class=""
+                                    style="float:right;color:black"><i class="fas fa-book-open"></i></a>
                             </p>
                         </div>
                     </div>
@@ -547,23 +556,28 @@
                 <div class="col-6" id="group-news">
                     <div class="row">
                         @if ($news_terbaru->count() != null)
-                            @foreach($news_terbaru as $key => $newss)
+                            @foreach ($news_terbaru as $key => $newss)
                                 @php
-                                    if($key == 0)continue;
+                                    if ($key == 0) {
+                                        continue;
+                                    }
                                 @endphp
                                 <div class="col-12">
                                     <div class="card card-news-lates" style="z-index:2;">
                                         <div class="row no-gutters">
                                             <!-- <img src="/img/berita1.jpg" class="col-4 card-img-lates" alt="..."> -->
-                                            <img src="{{$newss->thumbnail}}" class="col-4 card-img-lates" alt="...">
+                                            <img src="{{ $newss->thumbnail }}" class="col-4 card-img-lates" alt="...">
                                             <div class="col-8">
                                                 <div class="card-body text-bottom">
                                                     <!-- <h5 class="card-title-lates card-title"> Stay Safe and Healthy</h5> -->
-                                                    <h5 class="card-title-lates card-title">{{$newss->news_title_id}}</h5>
-                                                    <p class="news-time-lates"><?= date('M d, Y' , strtotime($newss->news_date)) ?>
-                                                    <a href="" class="" style="float:right;color:black"><i
-                                                    class="fas fa-book-open"></i></a>
-                                                </p>
+                                                    <h5 class="card-title-lates card-title">{{ $newss->news_title_id }}
+                                                    </h5>
+                                                    <p class="news-time-lates">
+                                                        <?= date('M d, Y', strtotime($newss->news_date)) ?>
+                                                        <a href="{{ route('detailNews', ['id' => $newss->id]) }}"
+                                                            class="" style="float:right;color:black"><i
+                                                                class="fas fa-book-open"></i></a>
+                                                    </p>
                                                 </div>
                                             </div>
                                         </div>
@@ -583,24 +597,28 @@
         <div class="container popular-card-news" style="z-index:2;">
             <div class="row lates-title" style="z-index:2;">
                 @if ($news_terlaris->count() != null)
-                    @foreach($news_terlaris as $key => $newsst)
+                    @foreach ($news_terlaris as $key => $newsst)
                         @php
-                            if($key > 3) continue;
+                            if ($key > 3) {
+                                continue;
+                            }
                         @endphp
                         <div class="col-12">
                             <div class="card card-news-popular" style="z-index:2;" id="popular">
                                 <div class="row no-gutters">
                                     <div class="col-6">
-                                            <!-- <img src="{{ asset('img/hijau-next-news.png') }}" class="card-img-top card-img-popular" alt="..."> -->
-                                            <img src="{{$newsst->thumbnail}}" class="card-img-top card-img-popular" alt="...">
+                                        <!-- <img src="{{ asset('img/hijau-next-news.png') }}" class="card-img-top card-img-popular" alt="..."> -->
+                                        <img src="{{ $newsst->thumbnail }}" class="card-img-top card-img-popular"
+                                            alt="...">
                                     </div>
                                     <div class="col-6">
                                         <div class="card-body text-bottom" style="z-index:2;">
                                             <!-- <p class="news-popular-title">News</p> -->
                                             <!-- <h5 class="card-title card-title-popular">Card title</h5> -->
                                             <p class="news-popular-title">News</p>
-                                            <h5 class="card-title card-title-popular">{{$newsst->news_title_id}}</h5>
-                                            <p class="news-time-popular"><?= date('M d, Y', strtotime($newsst->news_date)) ?>
+                                            <h5 class="card-title card-title-popular">{{ $newsst->news_title_id }}</h5>
+                                            <p class="news-time-popular">
+                                                <?= date('M d, Y', strtotime($newsst->news_date)) ?>
                                             </p>
                                         </div>
                                     </div>
@@ -611,37 +629,44 @@
                 @endif
                 <div id="more" style="display:none;">
                     @if ($news_terlaris->count() != null)
-                        @foreach($news_terlaris as $key => $newsst)
-                        @php
-                            if($key <= 3) continue;
-                        @endphp
-                        <div class="col-12">
-                            <div class="card card-news-popular" style="z-index:2;">
-                                <div class="row no-gutters">
+                        @foreach ($news_terlaris as $key => $newsst)
+                            @php
+                                if ($key <= 3) {
+                                    continue;
+                                }
+                            @endphp
+                            <div class="col-12">
+                                <div class="card card-news-popular" style="z-index:2;">
+                                    <div class="row no-gutters">
                                         <div class="col-6">
                                             <!-- <img src="https://img.buzzfeed.com/buzzfeed-static/static/2014-05/campaign_images/webdr08/1/1/25-crowd-pleasing-karaoke-songs-that-are-actually-2-25835-1398920438-12_dblbig.jpg" class="card-img-top card-img-popular" alt="..."> -->
-                                            <img src="{{$newsst->thumbnail}}" class="card-img-top card-img-popular" alt="...">
+                                            <img src="{{ $newsst->thumbnail }}" class="card-img-top card-img-popular"
+                                                alt="...">
                                         </div>
                                         <div class="col-6">
                                             <div class="card-body text-bottom" style="z-index:2;">
                                                 <p class="news-popular-title">News</p>
-                                                <h5 class="card-title card-title-popular">{{$newsst->news_title_id}}</h5>
+                                                <h5 class="card-title card-title-popular">{{ $newsst->news_title_id }}
+                                                </h5>
                                                 <!-- <h5 class="card-title card-title-popular">Card title</h5> -->
-                                                <p class="news-time-popular"><?= date('M d, Y', strtotime($newsst->news_date)) ?>
+                                                <p class="news-time-popular">
+                                                    <?= date('M d, Y', strtotime($newsst->news_date)) ?>
                                                 </p>
                                             </div>
                                         </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
                         @endforeach
                     @endif
                 </div>
 
                 @if ($news_terlaris->count() > 4)
                     <div class="col-12">
-                        <button href="{{ url('/') }}" onclick="buttonLess()" id="myBtn" class="btn btn-primary">Load more</button>
-                        <button href="{{ url('/') }}" onclick="buttonLess()" id="myBtn2" class="btn btn-primary" style="display:none;">Close</button>
+                        <button href="{{ url('/') }}" onclick="buttonLess()" id="myBtn" class="btn btn-primary">Load
+                            more</button>
+                        <button href="{{ url('/') }}" onclick="buttonLess()" id="myBtn2" class="btn btn-primary"
+                            style="display:none;">Close</button>
                     </div>
                 @endif
             </div>
@@ -657,12 +682,12 @@
 
             if (moreDiv.style.display == "none") {
                 moreDiv.style.display = "block";
-                document.getElementById('myBtn').style.display='none';
-                document.getElementById('myBtn2').style.display='block';
+                document.getElementById('myBtn').style.display = 'none';
+                document.getElementById('myBtn2').style.display = 'block';
             } else {
                 moreDiv.style.display = "none";
-                document.getElementById('myBtn2').style.display='none';
-                document.getElementById('myBtn').style.display='block';
+                document.getElementById('myBtn2').style.display = 'none';
+                document.getElementById('myBtn').style.display = 'block';
             }
         }
     </script>
