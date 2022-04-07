b1be1f071dac5e2d1650503580762e644e4074d1
Lathif21 <lathif.sihab95@gmail.com>
2022-04-07 09:27:21 +0700

    add migartion and seeders

---
+++ database/seeders/SettingSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Settings;
+
+class SettingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Settings::create([
+            'key' => 'site_name',
+            'value' => 'Laravel\'s Blog',
+        ]);
+    }
+}
