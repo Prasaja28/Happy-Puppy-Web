857b3133242c6090fc1a5a86acc1339653eed897
Mukhammad Nur Prasaja <prasaja55@gmail.com>
2022-06-22 22:05:41 +0700

    update home dan dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -9,7 +9,22 @@
 @section('dashboard','active')
 @section('konten')
 <!-- Content Body place -->
-<h3>Hello world</h3>
+<h3>Haloo, {{ Session::get('username') }}</h3>
+<?php
+    date_default_timezone_set('Asia/Jakarta');
+
+    $Hour = date('G');
+
+    if ( $Hour >= 5 && $Hour <= 11 ) {
+    ?><?php ?><span style="color: black;font-size: 20px;"><?php echo "Selamat Pagi"; ?><?php ?></span><?php
+    } else if ( $Hour >= 12 && $Hour <= 14 ) {
+    ?><?php ?><span style="color: black;font-size: 20px;"><?php echo "Selamat Siang"; ?><?php ?></span><?php
+    } else if ( $Hour >= 15 || $Hour <= 17 ) {
+    ?><?php ?><span style="color: black;font-size: 20px;"><?php  echo "Selamat Sore"; ?><?php ?></span><?php
+    } else if ( $Hour >= 18 || $Hour <= 4 ) {
+    ?><?php ?><span style="color: black;font-size: 20px;"><?php  echo "Selamat Malam"; ?><?php ?></span><?php
+    }
+?>
 @endsection
 
 @section('script')
